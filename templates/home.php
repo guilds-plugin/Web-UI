5f062e62dc0ef2ff65aaa4daa96ba1b3185d8581
darbyjack <admin@glaremasters.me>
2019-10-14 08:53:37 -0500

    Added in bootstrap and started working on display more data.

--- templates/home.php
+++ templates/home.php
@@ -1,23 +1,35 @@
 <?php include('shared/header.html') ?>
 <main>
-	<h1>Guilds</h1>
-	<table>
-		<thead>
-			<tr>
-				<th>#</th>
-				<th>Name</th>
-				<th>Status</th>
-			</tr>
-		</thead>
-		<tbody>
-	    <?php foreach ($guilds as $guild_index => $guild): ?>
-	    	<tr>
-	    		<td><a href="/guild.php?id=<?= $guild_index ?>"><?= $guild_index ?></a></td>
-	    		<td><?= $guild->name ?></td>
-	    		<td><?= $guild->status ?></td>
-	    	</tr>
-	    <?php endforeach; ?>
-		</tbody>
-	</table>
+    <div class="container">
+        <div class="table-responsive">
+            <h1>Guilds</h1>
+            <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Members</th>
+                        <th>Status</th>
+                        <th>Creation Date</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach ($guilds as $guild_index => $guild): ?>
+                    <tr>
+                        <td><a href="/guild.php?id=<?= $guild->id ?>"><img style="width: 50px; height: 50px;" src="https://mc-heads.net/avatar/<?= $guild->guildMaster->uuid ?>/100"><?= $guild->name ?></a></td>
+                        <td>
+                            <?php foreach($guild->members as $member) : ?>
+                            <?= $member->uuid ?>
+                            <?php endforeach; ?>
+                        </td>
+                        <td><?= $guild->status ?></td>
+                        <td>
+                            <? echo date("m/d/Y H:i:s", $guild->creationDate / 1000); ?>
+                        </td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
 </main>
 <?php include('shared/footer.html') ?>
