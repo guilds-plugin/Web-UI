14390d9331a844e7e949cfb4d62083fa979a2f4d
Sander De la Marche <sanderdlm69@gmail.com>
2019-10-15 13:21:09 +0200

    Clean up leftover data in header template + correct favicon path

--- templates/shared/header.php
+++ templates/shared/header.php
@@ -2,11 +2,11 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Sander - Home</title>
+    <title>Guilds</title>
     <meta name="description" content="">
-    <meta name="author" content="Sander De la Marche">
+    <meta name="author" content="Guilds Project">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/png" href="rice-balls.png">
-    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
