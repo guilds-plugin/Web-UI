5bb494abc10380092236cf029ac4a48229611246
Sander De la Marche <sanderdlm69@gmail.com>
2019-10-15 13:35:58 +0200

    Alter footer text

--- templates/shared/footer.php
+++ templates/shared/footer.php
@@ -1,5 +1,5 @@
 <footer>
-    <p>This webpage is part of the Guilds project. You can report bugs or submit feature requests by making an issue in <a href="https://github.com/guilds-plugin/Web-UI">the repository</a> for this dashboard.</p>
+    <p>You can report bugs or submit feature requests by making an issue in <a href="https://github.com/guilds-plugin/Web-UI">the repository</a> for this project.</p>
 </footer>
 </body>
 </html>
