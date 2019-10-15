83e1bf453c46daa03f774e6ff0aabd86cbb9c886
Sander De la Marche <sanderdlm69@gmail.com>
2019-10-15 13:20:07 +0200

    Add a basic project description to the footer

--- templates/shared/footer.php
+++ templates/shared/footer.php
@@ -1,2 +1,5 @@
+<footer>
+    <p>This webpage is part of the Guilds project. You can report bugs or submit feature requests by making an issue in <a href="https://github.com/guilds-plugin/Web-UI">the repository</a> for this dashboard.</p>
+</footer>
 </body>
 </html>
