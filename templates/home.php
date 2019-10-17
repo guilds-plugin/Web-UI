f00bfc6d24dd3f22ae07e26ec02dbad1db767489
darbyjack <admin@glaremasters.me>
2019-10-17 14:55:07 -0500

    Added in some tooltips to the table labels.

--- templates/home.php
+++ templates/home.php
@@ -3,16 +3,16 @@
     <h1>Guilds</h1>
 </header>
 <main>
-    <table class="table">
+    <table class="table" id="board">
         <thead>
             <tr>
                 <th></th>
-                <th>Guild</th>
-                <th>Balance</th>
-                <th>Members</th>
-                <th>Tier</th>
-                <th>Arena Victories</th>
-                <th>Age</th>
+                <th aria-label="The name of the Guild" data-balloon-pos="up">Guild</th>
+                <th aria-label="The current bank balance of the Guild" data-balloon-pos="up">Balance</th>
+                <th aria-label="The amount of members in the Guild" data-balloon-pos="up">Members</th>
+                <th aria-label="The level of the Guild" data-balloon-pos="up">Tier</th>
+                <th aria-label="The number of war victories the Guild has" data-balloon-pos="up">Arena Victories</th>
+                <th aria-label="The age of the Guild" data-balloon-pos="up">Age</th>
             </tr>
         </thead>
         <tbody>
@@ -21,22 +21,22 @@
                 <td class="avatar">
                     <img class="guild-skull" src="https://mc-heads.net/avatar/<?= $guild->guildMaster->uuid ?>/100" alt="<?= $guild->name ?>'s avatar">
                 </td>
-                <td>
+                <td class="center">
                     <a href="/guild.php?id=<?= $guild->id ?>"><?= $guild->name ?></a>
                 </td>
-                <td>
+                <td class="center">
                     <?= $_ENV['CURRENCY_SYMBOL'] . number_format($guild->balance, $_ENV['DECIMAL_PLACES']) ?>
                 </td>
-                <td class="members">
+                <td class="center">
                     <?= count($guild->members) ?>
                 </td>
-                <td>
+                <td class="center">
                     <?= $guild->tier->level ?>
                 </td>
-                <td>
+                <td class="center">
                     <?php if ($guild->guildScore->wins == 0) { echo("0"); } else { echo($guild->guildScore->wins); } ?>
                 </td>
-                <td><?= $guild->age ?> day(s)</td>
+                <td class="center"><?= $guild->age ?> day(s)</td>
             </tr>
             <?php endforeach; ?>
         </tbody>
--- templates/shared/header.php
+++ templates/shared/header.php
@@ -8,5 +8,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/png" href="assets/img/favicon.png">
     <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/balloon.css">
 </head>
 <body>
